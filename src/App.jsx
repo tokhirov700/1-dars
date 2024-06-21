a2ed5d8993e62d943e455a2aa707ae96b2b39dff
Sarvar <162941642+tokhirov700@users.noreply.github.com>
2024-06-21 09:43:15 +0500

    second

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Header  from './components/header/index'
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <Header />
+         
+      </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/index.jsx
@@ -0,0 +1,10 @@
+import '..//../components/header/headr.css'
+function Header () {
+    return (
+        <header className="App-header">
+        <h1>Header</h1>
+      </header>
+    )
+}
+
+export default Header
